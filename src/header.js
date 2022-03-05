2ad8b0bdba8e6c27291a1cede6b349fb999d0c82
shakhzodbekbobobekov <shaxzodbekbobobekov@gmail.com>
2022-03-05 16:45:42 +0500

    react code

---
+++ src/header.js
@@ -0,0 +1,23 @@
+// import React from "react"
+
+// const Header = () => {
+//     return(
+//         <div className="header"> 
+//             <h1>Welcome to our Website</h1>
+//             <p>Discovore</p>
+//         </div>
+//     )
+// }
+
+// export default Header;
+
+
+import React from "react"
+
+const Header = () => {
+    return(
+        <h1>Header</h1>
+    )
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
+// import ReactDOM from "react-dom"
+// import React from "react"
+// import App from "./App"
+
+
+// ReactDOM.render(
+//    <div>
+//         <App></App>
+//    </div>
+// , document.getElementById('root'))
+
+
+
+
 import ReactDOM from "react-dom"
 import React from "react"
-
-
+import App from './App'
 
 ReactDOM.render(
-    <div className="container"> 
-        <h1>Hello guys!</h1>
-        <div className="box">
-            <p>
-                tushunarli text endi BablesJS bn
-            </p>
-        </div>
-
-    </div>
-    , document.getElementById('root'))
+   <div>
+     <App />
+   </div>,
+   document.getElementById('root'))
