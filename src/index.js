e43be517ae8f6eb80eb0f071e660c3cce093eea3
shakhzodbekbobobekov <shaxzodbekbobobekov@gmail.com>
2022-03-07 05:22:08 +0500

    get date

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
-// import ReactDOM from "react-dom"
-// import React from "react"
-// import App from "./App"
-
-
-// ReactDOM.render(
-//    <div>
-//         <App></App>
-//    </div>
-// , document.getElementById('root'))
-
-
-
-
 import ReactDOM from "react-dom"
 import React from "react"
-import App from './App'
+
+
+// const date = new Date()
+const currentTime = 99;
+
+let textDate = '';
+let styleColor = '';
+
+if( currentTime < 12 ){
+  textDate = 'Tong sahar';
+  styleColor = 'red';
+}else if( currentTime < 18 ){
+  textDate = 'Tushlik vaqti';
+  styleColor = 'yellow';
+}else{
+  textDate = 'Kechasi'
+  styleColor = 'blue'
+}
+
 
 ReactDOM.render(
    <div>
-     <App />
+     <h1 className="heading" style={styleColor} >
+       {textDate}
+      </h1> 
    </div>,
    document.getElementById('root'))
