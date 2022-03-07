d0fe7da92d0eae9d8397e52a7e55f52d153bdc87
shakhzodbekbobobekov <shaxzodbekbobobekov@gmail.com>
2022-03-07 07:59:11 +0500

    props practise

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import ReactDOM from "react-dom"
 import React from "react"
-import App from "./components/App"
 
+const Card = (props) => {
+  return (
+    <div>
+      <p>{props.names}</p>
+      <img width={200} src={props.src} alt="cr7" />
+      <p>{props.liked}</p>
+    </div>
+  )
+}
 
 
 ReactDOM.render(
-   <div>
-    <App />
-   </div>,
-   document.getElementById('root'))
+  <div>
+    <h1>Modern Best Movies</h1>
+
+    <Card names='Cristiano Ronaldo (Man Utd)' src='https://picsum.photos/200/300?random=2' liked='98% ❤' />
+    <Card names='Dostonbek' src='https://picsum.photos/200/300?random=1' liked='100% ❤' />
+    <Card names='Bexruz uzb' src='https://picsum.photos/200/300?random=3' liked='100% ❤' />
+  </div>,
+  document.getElementById('root'))
