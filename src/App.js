886d6cbdfccdd3e6b13dad08fa766e254aefb2d3
shakhzodbekbobobekov <shaxzodbekbobobekov@gmail.com>
2022-03-10 13:42:58 +0500

    counter numbers

--- src/App.js
+++ src/App.js
@@ -1,27 +1,63 @@
-import React from "react";
+// import React from "react";
 
+
+// class App extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             count: 0,
+//         }
+//     }
+
+//     addOne = () => {
+//         this.setState(({ count: this.state.count + 1 }), () => console.log('qushildi'));
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Cunter: {this.state.count} </h1>
+//                 <button onClick={this.addOne} > click++ </button>
+//                 <button> click-- </button>
+//             </div>
+//         )
+//     }
+// }
+
+// export default App; 
+
+
+import React from "react";
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             count: 0,
-        }
+        };
     }
 
     addOne = () => {
-        this.setState(({ count: this.state.count + 1 }), () => console.log('qushildi'));
+        this.setState({ count: this.state.count + 1 })
+    }
+
+    minus = () => {
+        if (this.state.count > 0) {
+            this.setState({ count: this.state.count - 1 })
+
+        }
     }
 
     render() {
         return (
             <div>
-                <h1>Cunter: {this.state.count} </h1>
-                <button onClick={this.addOne} > click++ </button>
-                <button> click-- </button>
+                <h1> Count:{this.state.count} </h1>
+                <button onClick={this.addOne} >Click Me ++</button>
+                <button onClick={this.minus} >Click Me --</button>
             </div>
         )
     }
+
 }
 
-export default App; 
+export default App
