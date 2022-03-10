70a73164ee073e000b6d802feee10b2d9410aee0
shakhzodbekbobobekov <shaxzodbekbobobekov@gmail.com>
2022-03-10 10:50:26 +0500

    state setState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import { Component } from "react"
+import React from "react";
 
-class App extends Component {
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            MaxsulotTuri: 'Temir',
+            Sifat: "Yangi",
+            Narxi: '50$'
+        };
+    }
+
+    changeNames = () => {
+        this.setState({
+            MaxsulotTuri: 'Taxta',
+            Sifat: "Eskiroq",
+            Narxi: '20$',
+        });
+    }
+
     render() {
         return (
             <div>
-                <h1>Hello Guys !</h1>
+                <p>MaxsulotTuri: {this.state.MaxsulotTuri} </p>
+                <p>Sifat: {this.state.Sifat}</p>
+                <p>Narxi: {this.state.Narxi}</p>
+                <button onClick={this.changeNames} > uzgar </button>
             </div>
         )
     }
 }
 
-
-export default App;
+export default App; 
