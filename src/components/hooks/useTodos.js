c9a2e17df9c0ceb161e9a71c20cb24d23aba6c04
shakhzodbekbobobekov <shaxzodbekbobobekov@gmail.com>
2022-06-05 14:17:13 +0500

    add useState hooks companents

---
+++ src/components/hooks/useTodos.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function useTodos() {
+  return <div>useTodos</div>;
+}
+
+export default useTodos;
---
+++ src/context/TodosContext.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TodosContext() {
+  return <div>TodosContext</div>;
+}
+
+export default TodosContext;
